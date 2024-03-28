7962f3374f0188105e915805b076776ce9d53031
Gabin Joyet <gjoyet@dimm06.univ-lr.fr>
2024-03-28 11:12:39 +0100

    [FIX] Lien fichier css

--- sae4app/src/composants/app/Menu.jsx
+++ sae4app/src/composants/app/Menu.jsx
@@ -18,7 +18,7 @@ const Menu = () =>{
                 <img src={image} alt='logo site'/>
                 <h2>SMART CAMPUS</h2>
                 {sessionStorage.getItem('role') !== "" ? <button onClick={handleretour} id="acces_u_btn">
-                    {sessionStorage.getItem('role') === "usager" ? <p>Retour</p> : <p>Deconnection</p>}
+                    {sessionStorage.getItem('role') === "usager" ? <p>Retour</p> : <p>Déconnexion</p>}
                 </button> : <></>}
             </header>
             <Outlet/>
