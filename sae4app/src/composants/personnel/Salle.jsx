e07f77ea46c371ec026c7a9cfd3a1b6591396433
Romain Moreau <rmorea05@dimm14.univ-lr.fr>
2024-03-28 10:05:37 +0100

    [ADD] Ajout de la porte

--- sae4app/src/composants/personnel/Salle.jsx
+++ sae4app/src/composants/personnel/Salle.jsx
@@ -1,29 +1,31 @@
 import PropTypes from "prop-types";
+import door from "../../assets/img/door.png"
 
 const Salle = (props) =>{
 
     if(props.nbAlerte24h >= 3) {
         return (
             <>
-                <button onClick={() => {props.redirection(props.id)}} className={"rouge"}>{props.nom} {props.nbAlerte24h} alertes</button>
+                <button onClick={() => {props.redirection(props.id)}} className={"rouge"}> <img src={door} alt={"ff"} className={"doorImg"} /> <span> {props.nom} </span><span>{props.nbAlerte24h} alertes</span></button>
             </>
             )
     }
     else if(props.nbAlerte24h == 1) {
         return (
             <>
-                <button onClick={() => {props.redirection(props.id)}} className={"orange"}>{props.nom} {props.nbAlerte24h} alerte </button>
+                <button onClick={() => {props.redirection(props.id)}} className={"orange"}> <img src={door} alt={"ff"} className={"doorImg"} /> <span>  {props.nom} </span> <span> {props.nbAlerte24h} alerte </span></button>
             </>)
     }
+
     else if(props.nbAlerte24h == 2) {
         return (
             <>
-                <button onClick={() => {props.redirection(props.id)}} className={"orange"}>{props.nom} {props.nbAlerte24h} alertes </button>
+                <button onClick={() => {props.redirection(props.id)}} className={"orange"}> <img src={door} alt={"ff"} className={"doorImg"} /> <span>{props.nom} </span><span>{props.nbAlerte24h} alertes </span></button>
             </>)
     }
     return (
         <>
-            <button onClick={() => {props.redirection(props.id)}} className={"vert"}>{props.nom} {props.nbAlerte24h} alerte</button>
+            <button onClick={() => {props.redirection(props.id)}} className={"vert"}> <img src={door} alt={"ff"} className={"doorImg"} /> <span> {props.nom} </span><span>{props.nbAlerte24h} alerte</span></button>
         </>
     )
 }
