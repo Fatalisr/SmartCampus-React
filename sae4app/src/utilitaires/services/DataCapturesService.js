14310e37f177286faec892567a37f31ea251c5e9
Ulysse Gay <ugay@dimm12.univ-lr.fr>
2024-03-28 10:30:05 +0100

    [ADD] Fonctions en charge de l'appel des requêtes GET de l'API Capture

--- sae4app/src/utilitaires/services/DataCapturesService.js
+++ sae4app/src/utilitaires/services/DataCapturesService.js
@@ -1,4 +1,6 @@
-import { useState, useEffect } from 'react';
+// Imports necessary for the page
+
+// Definition of constant variables
 
 const baseUrlApi = 'https://sae34.k8s.iut-larochelle.fr';
 const authentificationHeaders = {
@@ -7,37 +9,71 @@ const authentificationHeaders = {
     'userpass': 'sodqif-vefXym-0cikho'
 }
 
+// Functions
+
+/*
+ * getCaptures : asynchronous function that returns a promise containing json data
+ * Sends a HTTP GET request to the /api/captures route of the API Capture api.
+ */
 export const getCaptures = async() =>{
 
+    try{
+        const response = await fetch(`${baseUrlApi}/api/captures`,{
+            headers: authentificationHeaders
+        });
 
-    const response = await fetch(`${BASE_URL_API}/api/captures`,{
-        headers: AUTHENTIFICATION_HEADERS
-    });
-
-    return await response.json();
+        const jsonData = response.json();
+        return await jsonData;
+    }
+    catch (error){
+        throw `Error in getCaptures request : ${error.name}, ${error.message}` ;
+    }
 }
 
-export const getCapturesInterval = async() =>{
+/*
+ * getCapturesInterval : asynchronous function that returns a promise containing json data
+ * Sends a HTTP GET request to the /api/captures/interval route of the API Capture api.
+ * date1 parameter : starting date of the interval, corresponds to the date1 required query parameter of the route
+ * date1 parameter : ending date of the interval, corresponds to the date2 required query parameter of the route
+ */
+export const getCapturesInterval = async(date1, date2) =>{
 
-    const startDateQuery = 'date1='${date1.toString()};
-    const endDateQuery = 'date2='${date2.toString()};
+    const startDateQuery = `date1=${date1.toString()}`;
+    const endDateQuery = `date2=${date2.toString()}`;
 
-    const response = await fetch(`${BASE_URL_API}/api/captures/interval?${startDateQuery}&${endDateQuery}`,{
-        headers: AUTHENTIFICATION_HEADERS
-    });
+    try {
+        const response = await fetch(`${baseUrlApi}/api/captures/interval?${startDateQuery}&${endDateQuery}`, {
+            headers: authentificationHeaders
+        });
 
-    return await response.json();
+        const jsonData = response.json();
+        return await jsonData;
+    }
+    catch (error){
+        throw `Error in getCapturesInterval request : ${error.name}, ${error.message}` ;
+    }
 }
 
-export const getLastCaptures = async(lines) =>{
+/*
+ * getLastCaptures : asynchronous function that returns a promise containing json data
+ * Sends a HTTP GET request to the /api/captures/last route of the API Capture api.
+ * limit parameter : number of captures awaited, corresponds to the limit query parameter of the route
+ */
+export const getLastCaptures = async(lines)=>{
 
-    const limitQuery = 'limit='${lines.toString()};
+    const limitQuery = `limit=${lines.toString()}`;
 
-    const response = await fetch(`${BASE_URL_API}/api/captures/last?${limitQuery}`,{
-        headers: AUTHENTIFICATION_HEADERS
-    });
+    try {
+        const response = await fetch(`${baseUrlApi}/api/captures/last?${limitQuery}`, {
+            headers: authentificationHeaders
+        });
 
-    return await response.json();
+        const jsonData = response.json();
+        return await jsonData;
+    }
+    catch (error){
+        throw `Error in getCapturesLast request : ${error.name}, ${error.message}` ;
+    }
 }
 
 
