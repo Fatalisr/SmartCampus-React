d841f41d0f1cffa9907725fcdfe917dda473e862
Gabin Joyet <gjoyet@dimm06.univ-lr.fr>
2024-03-28 09:44:12 +0100

    [ADD] Composant Intervention

--- sae4app/src/composants/technicien/Intervention.jsx
+++ sae4app/src/composants/technicien/Intervention.jsx
@@ -1,13 +1,39 @@
 import PropTypes from "prop-types";
+import '../../assets/css/technicien/technicien.css'
 
 const Intervention = (props) =>{
-    return(
-        <>
-        </>
-    )
+    if(props.typeIntervention == "A_INSTALLER"){
+        return(
+            <>
+                <div className={"flRow maint"}>
+                    <p>{props.SAName}</p>
+                    <p>Maintenance</p>
+                    <p>{props.salle}</p>
+                    {props.techID != undefined?<p>∅</p>: <p>{props.techID}</p>}
+                </div>
+            </>
+        )
+    }
+    if(props.typeIntervention == "MAINTENANCE"){
+        return(
+            <>
+                <div className={"flRow maint"}>
+                    <p>{props.SAName}</p>
+                    <p>Installation</p>
+                    <p>{props.salle}</p>
+                    {props.techID != undefined?<p>∅</p>: <p>{props.techID}</p>}
+                </div>
+            </>
+        )
+    }
+
 }
 
 Intervention.propTypes = {
-    exampleProp: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    SAName: PropTypes.string.isRequired,
+    techID: PropTypes.number.isRequired,
+    typeIntervention: PropTypes.string.isRequired,
+    salle: PropTypes.string.isRequired,
 }
 export default Intervention
