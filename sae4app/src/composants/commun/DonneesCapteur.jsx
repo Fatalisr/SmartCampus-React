cd9fb036a9aeae39827f9fde5f43ffcc883b93af
Ulysse Gay <ugay@dimm12.univ-lr.fr>
2024-04-02 17:14:46 +0200

    [ADD] Affichage des conseils en fonction des données remontées

--- sae4app/src/composants/commun/DonneesCapteur.jsx
+++ sae4app/src/composants/commun/DonneesCapteur.jsx
@@ -18,10 +18,9 @@ const DonneesCapteur = (props) =>{
         Promise.all([fetchtemp(), fetchhumid(), fetchCO2()]).then((values) => {
             setData(values);
         });
-    }, []);
+    }, [props.name]);
 
     const renderData = datas.map((data) => {
-        console.log(data)
         if(data[0].localisation === props.name) {
             return <div key={data[0].id} className="DonnesCapteurline">{data[0].nom} - {data[0].valeur}</div>;
         }
