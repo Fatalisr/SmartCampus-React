fd5053a804b8fc3526e983dd5415d58daad0be77
Ulysse Gay <ugay@dimm12.univ-lr.fr>
2024-04-03 17:38:25 +0200

    [ADD] Commentaires sur la validité des données

--- ARCHI/src/Co2/co2.cpp
+++ ARCHI/src/Co2/co2.cpp
@@ -8,7 +8,9 @@
 /*                           Fonctions                             */
 /*-----------------------------------------------------------------*/
 
-// Fonction d'initialisation du capteur
+/*
+ * Fonction initCO2Sensor() : initialise le capteur de CO2 SGP30 conformément à l'exemple
+ */
 void initCO2Sensor()
 {
   s16 err;
@@ -35,7 +37,10 @@ void initCO2Sensor()
   ledCO2Ok = true;
 }
 
-// Fonction de lecture du capteur de CO2
+/*
+ * Fonction getCO2Value(u16 &ppm) : Récupère le taux de CO2 captée par le SGP30 et l'attribue au paramètre ppm passé en référence.
+ * La fonction ne renvoie aucune valeur.
+ */
 s16 getCO2Value(u16 &ppm)
 {
   s16 err = 0;
@@ -52,6 +57,12 @@ s16 getCO2Value(u16 &ppm)
   return err;
 }
 
+/*
+ * Fonction getValidityCO2(u16 &ppm) : Vérifie la validité du taux de CO2 passé en paramètre. Si elle n'est pas valide, on retente de récupérer une nouvelle valeur.
+ * Renvoie un booléen (false si la donnée n'est pas valide, true si elle l'est)
+ * Un taux de CO2 est valide s'il est situé entre 400 et 5000 ppm.
+ * param ppm : taux de CO2 dont on vérifie la validité. Passé par référence, sa valeur est modifié lorsque l'on récupère une nouvelle valeur.
+ */
 bool getValidityCO2(u16 &ppm)
 {
   if(ppm >= 400 or ppm < 5000)
@@ -71,11 +82,16 @@ bool getValidityCO2(u16 &ppm)
 
 // Tache de lecture du CO2
 void getCO2Task(void *parameter){
-  err_CO2 = getCO2Value(ppm); 
-  vTaskDelay(pdMS_TO_TICKS( 15000 )); 
+    err_CO2 = getCO2Value(ppm);
     for(;;){
         err_CO2 = getCO2Value(ppm);
         
+        //============================================//
+        //           Validité de la donnéee           //
+        //============================================//
+
+        // Si la valeur d'humidité est incorrecte, on réessaye de capter une donnée correcte
+        // Tant que la donnée n'est pas valide et que le nombre de tentative ne dépasse pas le nombre de tentatives de rectification de la donnée fixé
         int retryCO2 = 0;
         while(!getValidityCO2(ppm) and retryCO2 < 2){
           
@@ -86,8 +102,13 @@ void getCO2Task(void *parameter){
           Serial.println(retryCO2);
           Serial.println(ppm);
         }
+
+
+        //===========================================//
+        //     Envoi et persistence de la donnée     //
+        //===========================================//
         
-        if(getValidityCO2)
+        if(getValidityCO2(ppm))  // La donnée est valide
         {
           compteurCo2 += 1;
           sommeCo2 += ppm;
@@ -105,7 +126,7 @@ void getCO2Task(void *parameter){
             Serial.println("CO2 : Ecart trop faible avec valeur precedente, la capture n'a pas ete envoyee");
           }
         }
-        else
+        else  // La donnée n'est pas valide
         {
           Serial.println("CO2 non-envoyé car la valeur n'est pas valide :");
           Serial.println(ppm);
--- ARCHI/src/TemperatureHumidite/temperatureHumidite.cpp
+++ ARCHI/src/TemperatureHumidite/temperatureHumidite.cpp
@@ -2,7 +2,6 @@
 /*                            Include                              */
 /*-----------------------------------------------------------------*/
 
-// Fichier du projet
 #include "temperatureHumidite.h"
 
 /*-----------------------------------------------------------------*/
@@ -16,7 +15,10 @@ DHT_Unified dht(DHTPIN, DHTTYPE);// Instance du capteur de Hum/Temp
 /*                           Fonctions                             */
 /*-----------------------------------------------------------------*/
 
-// Initiation du capteur
+/*
+ * Fonction initTempHumSensor() : initialise le capteur DHT
+ * Appel la fonction begin du capteur et confirme le bon déroulement de l'initialisation
+ */
 void initTempHumSensor()
 {
     // Initialize temperature sensor
@@ -24,66 +26,99 @@ void initTempHumSensor()
     Serial.println("Init Hum/Temp OK");
 }
 
+/*
+ * Fonction getTempValue(float & temp) : Récupère la température captée par le DHT et l'attribue au paramètre temp passé en référence
+ * La fonction ne renvoie aucune valeur.
+ */
 void getTempValue(float & temp){
+    
+    // Evènement associé au capteur
     sensors_event_t event;
 
-    // Get temperature event and print its value.
-    dht.temperature().getEvent(&event); 
+    // Récupère l'évènement
+    dht.temperature().getEvent(&event);
+    // Vérification de la valeur contenue par l'évènement (Si l'évènement ne contient pas de température, on prévient qu'il y a une erreur)
     if (isnan(event.temperature)) {
         Serial.println(F("Error reading temperature!"));
         ledTempOk = false; 
     }
+    // L'évènement contient une température
     else {
-        temperature = event.temperature;
+        temperature = event.temperature; // On stocke la valeur récupérée par l'évènement dans la variable temperature (déclarée dans /src/variables.h)
         ledTempOk = true; 
     }
 }
 
-// fonction de capture de l'humidité et de la temp
+/*
+ * Fonction getHumValue(float & hum) : Récupère l'humidité captée par le DHT et l'attribue au paramètre hum passé en référence
+ * La fonction ne renvoie aucune valeur.
+ */
 void getHumValue(float & hum){
+    
+    // Evènement associé au capteur
     sensors_event_t event;
 
-    // Get humidity event and print its value.
+    // Récupère l'évènement
     dht.humidity().getEvent(&event);
+    // Vérification de la valeur contenue par l'évènement (Si l'évènement ne contient pas de valeur d'humidité, on prévient qu'il y a une erreur)
     if(isnan(event.relative_humidity)){
         Serial.println(F("Error reading humidity!"));
         ledHumiOk = false; 
     }
+    // L'évènement contient une donnée d'humidité
     else {
-        humidity = event.relative_humidity;
+        humidity = event.relative_humidity; // On stocke la valeur récupérée par l'évènement dans la variable humidity (déclarée dans /src/variables.h)
         ledHumiOk = true; 
     }
 }
 
+/*
+ * Fonction getValidityTemp(float & temp) : Vérifie la validité de la température passée en paramètre. Si elle n'est pas valide, on retente de récupérer une nouvelle valeur.
+ * Renvoie un booléen tempValid (false si la donnée n'est pas valide, true si elle l'est)
+ * Une température est valide si elle est située entre -20 et 60°C.
+ * param temp : température dont on vérifie la validité. Passé par référence, sa valeur est modifié lorsque l'on récupère une nouvelle valeur de température.
+ */
 bool getValidityTemp(float & temp){
 
+  // Booléen attestant de la validité de la température
   bool tempValid = false;
-
+  
+  // La température stockée dans temp est valide
   if(temperature > -20 and temperature < 60)
   {
     tempValid = true;
   }
-
+  
+  // La température n'est pas valide ...
   if(!tempValid)
   {
-    getTempValue(temp);
+    getTempValue(temp); // ... on récupère une nouvelle valeur.
   }
 
   return tempValid;
 }
 
+/*
+ * Fonction getValidityHum(float & hum) : Vérifie la validité de l'humidité passée en paramètre. Si elle n'est pas valide, on retente de récupérer une nouvelle valeur.
+ * Renvoie un booléen humValid (false si la donnée n'est pas valide, true si elle l'est)
+ * Une donnée d'humidité est valide si elle est située entre 5 et 99%.
+ * param hum : donnée d'humidité dont on vérifie la validité. Passé par référence, sa valeur est modifié lorsque l'on récupère une nouvelle valeur d'humidité.
+ */
 bool getValidityHum(float & hum)
 {
+  // Booléen attestant de la validité de l'humidité
   bool humValid = false;
-
+  
+  // L'humidité stockée dans hum est valide
   if(hum > 5 and hum < 99)
   {
     humValid = true;
   }
-
+  
+  // La donnée n'est pas valide ...
   if(!humValid)
   {
-    getHumValue(hum);
+    getHumValue(hum); // ... on récupère une nouvelle valeur.
   }
 
   return humValid;
@@ -95,37 +130,48 @@ bool getValidityHum(float & hum)
 
 // Capture les valeurs et incremente les variables globales correspondante
 void getHumTempTask(void *parameter){
-    getHumValue(humidity);
-    getTempValue(temperature);
-    vTaskDelay(pdMS_TO_TICKS( 20000 )); 
+
     for(;;){
+        // Première récupération de l'humidité et de la température
         getHumValue(humidity);
         getTempValue(temperature);
+
+        //============================================//
+        //           Validité de la donnéee           //
+        //============================================//
+
+        // On vérifie une première fois la validité des données, qui sera stockée dans les variable humValid et tempValid
         bool humValid = getValidityHum(humidity);
         bool tempValid = getValidityTemp(temperature);
 
+        // Si la valeur d'humidité est incorrecte, on réessaye de capter une donnée correcte
+        // Tant que la donnée n'est pas valide et que le nombre de tentative ne dépasse pas le nombre de tentatives de rectification de la donnée fixé
         int retryHum = 0;
-        
         while(!humValid or retryHum < 2){
-            vTaskDelay( pdMS_TO_TICKS( 2000 ) );
-            humValid = getValidityHum(humidity);
-            retryHum += 1;
+            vTaskDelay( pdMS_TO_TICKS( 2000 ) );  // On attend 2 secondes avant de récupérer une nouvelle donnée
+            humValid = getValidityHum(humidity);  // On vérifie à nouveau la validité
+            retryHum += 1;                        // On incrémente le nombre de tentatives effectuées
         }
 
+        // Même chose pour la température
         int retryTemp = 0;
         while(!tempValid or retryTemp < 2){
             vTaskDelay( pdMS_TO_TICKS( 2000 ) );
             tempValid = getValidityTemp(temperature);
-
             retryTemp += 1;
         }
         
-        if(humValid)
+        //===========================================//
+        //     Envoi et persistence de la donnée     //
+        //===========================================//
+
+        if(humValid)    // La donnée d'humidité est valide
         {
             compteurHum += 1;
             sommeHum += humidity;
             Serial.println("Humidite valide :");
             Serial.println(humidity);
+
             if(humidity < humidityEnvoye - ecartHum or humidity > humidityEnvoye + ecartHum)
             {
                 humidityEnvoye = humidity;
@@ -144,9 +190,9 @@ void getHumTempTask(void *parameter){
             Serial.println(humidity);
         }
 
-        if(tempValid)
+        if(tempValid)   // La donnée de température est valide
         {
-            Serial.println("Temperature valide :");
+            Serial.println("Temperature valide");
             Serial.println(temperature);
             compteurTemp += 1;
             sommeTemp += temperature;
@@ -162,7 +208,7 @@ void getHumTempTask(void *parameter){
                 Serial.println("Temperature : Ecart trop faible avec valeur precedente, la capture n'a pas ete envoyee");
             }
         }
-        else
+        else    // La donnée de température n'est pas valide
         {
             Serial.println("Temperature non-envoyee car la valeur n'est pas valide :");
             Serial.println(temperature);
